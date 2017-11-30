07ec5cc04874325e75297602fef76e63afde1e10
Loëck Vézien <loeck.vezien@ekino.com>
2017-11-30 18:04:03 +0100

    Oooops

--- src/components/Html.js
+++ src/components/Html.js
@@ -16,7 +16,7 @@ const Html = ({
       <title>{title}</title>
 
       <meta charSet="utf-8" />
-      <meta http-equiv="x-ua-compatible" content="ie=edge" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
 
       <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
