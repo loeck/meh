8b2c08c2ada75ccb5993deea6c600bc1efc9a6c1
Loëck Vézien <loeck.vezien@ekino.com>
2017-12-09 22:48:36 +0100

    Use injectGlobal for App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react'
 
+import { injectGlobal } from 'styled-components'
 import { Switch, Route } from 'react-router'
 import { Provider } from 'react-redux'
 
 import routes from 'routes'
+
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  * {
+    -webkit-font-smoothing: antialiased;
+    background: transparent;
+    border: none;
+    box-sizing: border-box;
+    color: inherit;
+    font: inherit;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 13px;
+  }
+`
 
 export default (store, Router, routerProps) => (
   <Provider store={store}>
--- src/components/Html.js
+++ src/components/Html.js
@@ -3,8 +3,6 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import serialize from 'serialize-javascript'
 
-import defaultStyles from 'helpers/defaultStyles'
-
 import webpackConfig from '../../webpack/base'
 
 const Html = ({
@@ -22,9 +20,6 @@ const Html = ({
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/assets/favicon.ico" type="image/x-icon" />
-      <style
-        dangerouslySetInnerHTML={{ __html: defaultStyles }} // eslint-disable-line react/no-danger
-      />
       {styles}
       <script
         dangerouslySetInnerHTML={{ __html: `window.__INITIAL_STATE__ = ${serialize(state)}` }} // eslint-disable-line react/no-danger
@@ -60,7 +55,7 @@ Html.propTypes = {
     vendor: PropTypes.string,
     manifest: PropTypes.string,
   }),
-  styles: PropTypes.element,
+  styles: PropTypes.node,
   title: PropTypes.string,
 }
 
